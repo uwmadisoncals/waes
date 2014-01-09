73f025298b61ce301f16312fd94c10feee7b3ead
Rich Donohue <rgdonohue@gmail.com>
2014-01-09 14:56:10 -0600

    remove explore template part

---
+++ page.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ * @package WordPress
+ * @subpackage CALSv1
+ * @since CALS 1.0
+ */
+
+get_header(); ?>
+
+
+	<div id="main">
+
+		<div id="primary">
+		
+			<div id="content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+					<!-- <?php get_template_part('nav_menu', 'explore');?> -->
+
+					<?php //comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+				
+			</div><!-- #content -->
+			<?php get_sidebar(); ?>
+			<div class="clear"></div>
+			
+		</div><!-- #primary -->
+
+	</div>
+<?php get_footer(); ?>
+
+</div>
